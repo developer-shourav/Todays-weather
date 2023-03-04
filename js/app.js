2cbc63fa35b2c697d441e819a8879bff28f9b0c7
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-04 07:01:23 +0600

    weather api added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("Calling From App.js");
+
+const loadWeatherData = (city) => {
+    const API_KEY = "45aa5629fd856daff54cff3b43dd14e6";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+loadWeatherData('dhaka')
